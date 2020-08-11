aceee5c1782be42cbbae4a3ad499990581a066c9
DivyaNidadavolu26 <34182120+DivyaNidadavolu26@users.noreply.github.com>
2020-08-11 15:06:54 +0200

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Handler handler;
+    Runnable runnable;
+    ImageView img;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageView);
+        img.animate().alpha(4000).setDuration(0);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dep = new Intent( MainActivity.this, LoginActivity.class);
+                startActivity(dep);
+                finish();
+            }
+        }, 4000);
+
+    }
+}
